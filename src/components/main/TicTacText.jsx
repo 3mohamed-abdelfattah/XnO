2a8d31ac160a9c668f2133b941aa88949fe10879
MohamedMohamed <m01126165016@icloud.com>
2024-09-07 05:28:22 +0300

    Edit Logo Responsive Issue

--- src/components/main/TicTacText.jsx
+++ src/components/main/TicTacText.jsx
@@ -1,12 +1,9 @@
+import { TicTacToe } from "@/utils/icon.util"
+
 export const TicTacText = () => {
-
-    const styles = {
-        backgroundText: 'absolute bottom-5 -left-6 vs:text-[200px]/[110px] text-[150px]/[90px]'
-    }
-
     return (
-        <p className={styles.backgroundText}>
-            tic. <br /><span className='relative text-secondary z-50 '>tac.<br /></span>toe.
-        </p>
+        <div className='absolute bottom-0'>
+            <TicTacToe />
+        </div>
     )
 }
